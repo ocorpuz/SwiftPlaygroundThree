5a3b2ad81a010ff43a68c9db11c07165a991d327
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-02-18 10:20:06 -1000

    Part 1-7 Completed

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+
+
+
+//Part 3
+
+var fruitNames = [" Kiwis", " Oranges", " Bananas"]
+
+//Part 4
+
+print (fruitNames)
+
+//Part 5
+
+/*Desired Results
+ I like to eat kiwis
+ I like to eat Oranges"
+ etc
+ 
+ */
+//experiment 1
+print ("I like to eat \(fruitNames)")
+
+//loop
+//"name" is a temporary container that holds everything in the array
+// can put anything "for _ in _"
+//print ("_" + _ )
+
+for name in fruitNames
+{
+    print ("I like to eat" + name)
+}
+
+//Part 7
+
+print (fruitNames[1])
+
+//print index 0
+
+print (fruitNames[0])
+
+
+
